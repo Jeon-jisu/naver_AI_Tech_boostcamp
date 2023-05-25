08ad46e729a031a562effa9e193d213c9fb0244f
Jeon-jisu <wltnjeon0119@gmail.com>
2023-05-25 11:48:47 +0900

    ✏️ add: update 0525 boj

--- boj_practice/w12/1012.py
+++ boj_practice/w12/1012.py
@@ -1,24 +1,29 @@
 t = int(input())
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+def BFS(x,y):
+    queue = [(x,y)]
+    nong[x][y] = 0 
+    while queue:
+        x,y = queue.pop(0)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= m or ny <0 or ny >=n:
+                continue
+            if nong[nx][ny] == 1:
+                queue.append((nx,ny))
+                nong[nx][ny] = 0
 for i in range(t):
-    new = []
-    old = []
-    n,m,k = map(int, input().split())
+    m,n,k = map(int,input().split())
+    nong = [[0 for a in range(n)]for b in range(m)]
     cnt = 0
-    newcnt = k
-    dellist = []
-    batlist = [[0 for t in range(n)] for i in range(m)]
-    for j in range(k):
-        x, y = map(int, input().split())
-        # batlist[x][y] = 1
-        new.append([x,y])
-        old.append([x,y])
-    for t in range(k):
-        if [new[t][0]+1,new[t][1]] in new:
-            if [new[t][0]+1,new[t][1]] not in dellist:
-                newcnt-=1#오른쪽 한칸 검사
-                dellist.append([new[t][0]+1,new[t][1]])
-        if [new[t][0],new[t][1]+1] in new:
-            if [new[t][0],new[t][1]+1] not in dellist:
-                newcnt-=1#오른쪽 한칸 검사
-                dellist.append([new[t][0],new[t][1]+1])
-    print(newcnt)
+    for ek in range(k):
+        ix,iy = map(int,input().split())
+        nong[ix][iy] = 1
+    for a in range(m):
+        for b in range(n):
+            if nong[a][b] == 1:
+                BFS(a,b)
+                cnt+=1
+    print(cnt)
